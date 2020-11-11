4a9958f99652846897071664ab23ba57b6b3dd7b
Alessio Treglia <alessio@tendermint.com>
2020-11-11 12:13:39 +0000

    add test cases

--- libs/os/os_test.go
+++ libs/os/os_test.go
@@ -1,11 +1,14 @@
-package os
+package os_test
 
 import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"testing"
+
+	tmos "github.com/tendermint/tendermint/libs/os"
 )
 
 func TestCopyFile(t *testing.T) {
@@ -20,7 +23,7 @@ func TestCopyFile(t *testing.T) {
 	}
 
 	copyfile := fmt.Sprintf("%s.copy", tmpfile.Name())
-	if err := CopyFile(tmpfile.Name(), copyfile); err != nil {
+	if err := tmos.CopyFile(tmpfile.Name(), copyfile); err != nil {
 		t.Fatal(err)
 	}
 	if _, err := os.Stat(copyfile); os.IsNotExist(err) {
@@ -34,4 +37,66 @@ func TestCopyFile(t *testing.T) {
 		t.Fatalf("copy file content differs: expected %v, got %v", content, data)
 	}
 	os.Remove(copyfile)
+}
+
+func TestMustWriteFile(t *testing.T) {
+	tmpdir := t.TempDir()
+	if os.Getenv("TM_MUST_WRITE_FILE_TEST") == "1" {
+		t.Log("inside test process")
+		tmos.MustWriteFile(tmpdir, []byte("test"), 0644)
+		return
+	}
+
+	cmd, _, _ := newTestProgram(t, "TM_MUST_WRITE_FILE_TEST")
+
+	err := cmd.Run()
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+
+	e, ok := err.(*exec.ExitError)
+	if !ok {
+		t.Fatal("this error should not be triggered")
+	}
+
+	if e.ExitCode() != 1 {
+		t.Fatalf("wrong exit code, want 1, got %d", e.ExitCode())
+	}
+}
+
+func TestMustReadFile(t *testing.T) {
+	tmpdir := t.TempDir()
+	if os.Getenv("TM_MUST_WRITE_FILE_TEST") == "1" {
+		t.Log("inside test process")
+		tmos.MustReadFile(tmpdir)
+		return
+	}
+
+	cmd, _, _ := newTestProgram(t, "TM_MUST_WRITE_FILE_TEST")
+
+	err := cmd.Run()
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+
+	e, ok := err.(*exec.ExitError)
+	if !ok {
+		t.Fatal("this error should not be triggered")
+	}
+
+	if e.ExitCode() != 1 {
+		t.Fatalf("wrong exit code, want 1, got %d", e.ExitCode())
+	}
+}
+
+func newTestProgram(t *testing.T, environVar string) (cmd *exec.Cmd, stdout *bytes.Buffer, stderr *bytes.Buffer) {
+	t.Helper()
+
+	cmd = exec.Command(os.Args[0], "-test.run="+t.Name())
+	stdout, stderr = bytes.NewBufferString(""), bytes.NewBufferString("")
+	cmd.Env = append(os.Environ(), fmt.Sprintf("%s=1", environVar))
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	return
 }
